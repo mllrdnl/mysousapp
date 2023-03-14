c0c2749d8668e937337d7889665baf27352ad8b5
Mallory Daniel <mllr.dnl@gmail.com>
2023-03-14 09:40:04 -0700

    initial commit

---
+++ models/ingredient.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const ingredientSchema = new mongoose.Schema({
+  name: String,
+  name_scientific: String,
+  description: String,
+  food_group: String,
+  food_subgroup: String,
+  timestamps: true,
+});
+
+export default mongoose.models.ingredient ||
+  mongoose.model("ingredient", ingredientSchema);
